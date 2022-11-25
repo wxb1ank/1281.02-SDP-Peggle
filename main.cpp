f12e9a3cedbd1ce2dcea1f43c2d7290b7732f118
wxb1ank <wxblank@gmail.com>
2022-11-25 13:56:51 -0500

    Make 'will' branch

--- main.cpp
+++ main.cpp
@@ -10,60 +10,147 @@
 
 #define GRAVITY_ACCEL 200'000.
 
-#define BALL_RADIUS 10
-
-#define MIN_BALL_Y ((float)BALL_RADIUS)
-#define MAX_BALL_Y (((float)HEIGHT - 1.0) - (float)BALL_RADIUS)
-#define MIN_BALL_X ((float)BALL_RADIUS)
-#define MAX_BALL_X (((float)WIDTH - 1.0) - (float)BALL_RADIUS)
+#define MIN_BALL_Y ((float)Ball::RADIUS)
+#define MAX_BALL_Y (((float)HEIGHT - 1.0) - (float)Ball::RADIUS)
+#define MIN_BALL_X ((float)Ball::RADIUS)
+#define MAX_BALL_X (((float)WIDTH - 1.0) - (float)Ball::RADIUS)
 #define WIDTH  320
 #define HEIGHT 240
 
-#define INIT_VELOCITY 10'000.
+bool is_lcd_touched() {
+    float trash[2];
+    return LCD.Touch(&trash[0], &trash[1]);
+}
+
+struct Position {
+    float x;
+    float y;
+
+    static Position target() {
+        while (!is_lcd_touched());
+
+        Position target;
+        while (LCD.Touch(&target.x, &target.y));
+
+        return target;
+    }
+
+    float angle_to(const Position other) const {
+        const float x = other.x - this->x;
+        const float y = other.y - this->y;
+
+        const float angle = M_PI_2 - atanf(x / y);
+        printf("%f deg\n", angle * (180. / M_PI));
+
+        return angle;
+    }
+};
+
+struct Velocity {
+    float x;
+    float y;
+
+    float magnitude() const {
+        return std::abs(hypotf(this->x, this->y));
+    }
+};
+
+class Ball {
+public:
+    static constexpr size_t RADIUS{4};
+
+    void shoot(const float angle) {
+        const float mag = this->vel.magnitude();
+        this->vel.x = mag * cos(angle);
+        this->vel.y = mag * sin(angle);
+    }
+
+    void shoot_at(const Position target) {
+        this->shoot(this->pos.angle_to(target));
+    }
+
+    void tick() {
+        this->tick_x();
+        this->tick_y();
+
+        // TODO
+
+
+        if (this->pos.y < MIN_BALL_Y) {
+            this->pos.y = MIN_BALL_Y;
+            this->vel.y *= -0.75;
+        } else if(this->pos.y > MAX_BALL_Y) {
+            this->pos.y = MAX_BALL_Y;
+            this->vel.y *= -0.75;
+        }
+
+        if (this->pos.x < MIN_BALL_X) {
+            this->pos.x = MIN_BALL_X;
+            this->vel.x *= -0.75;
+        } else if (this->pos.x > MAX_BALL_X) {
+            this->pos.x = MAX_BALL_X;
+            this->vel.x *= -0.75;
+        }
+    }
+
+    void draw() const {
+        LCD.DrawCircle((int)this->pos.x, (int)this->pos.y, (int)Ball::RADIUS);
+    }
+
+private:
+    Position pos{(float)WIDTH / 2., 0.};
+    Velocity vel{0., 10'000.};
+
+    void tick_x() {
+        this->pos.x += this->vel.x * TICK_DURATION;
+    }
+
+    void tick_y() {
+        this->vel.y += GRAVITY_ACCEL * TICK_DURATION;
+        this->pos.y += this->vel.y * TICK_DURATION;
+    }
+};
+
+class HasHitbox {
+public:
+    virtual bool collides_with(const Ball &);
+
+    virtual Velocity deflection();
+};
+
+class Wall : HasHitbox {
+
+};
+
+class Peg : HasHitbox {
+public:
+    Peg();
+    Peg(int x_pos, int y_pos, int radius);
+    void collisionCheck(float x, float y, float * vx, float * vy);
+    int getX();
+    int getY();
+    int getRadius();
+    int getStatus();
+private:
+    int x_position, y_position, peg_radius, active;
+};
 
 int main()
 {
-    float target_x;
-    float target_y;
-
-    while (!LCD.Touch(&target_x, &target_y));
-
-    target_x -= ((float)WIDTH / 2.);
-    printf("x:%f\ny:%f\n",target_x,target_y);
-    float angle = -1.*atanf(target_x / target_y) + M_PI_2;
-    printf("Angle: %f\n", angle * (180. / M_PI));
-    float vx = INIT_VELOCITY*cos(angle);
-    float vy = INIT_VELOCITY*sin(angle);
-
-    float x = (float)WIDTH / 2.;
-    float y = 0.;
+    Ball ball;
+    ball.draw();
+    ball.shoot_at(Position::target());
 
     while (true) {
         LCD.Clear(BLACK);
 
-        x += vx*TICK_DURATION;
-
-        vy += GRAVITY_ACCEL*TICK_DURATION;
-        y += vy*TICK_DURATION;
-
-        if (y < MIN_BALL_Y) {
-            y = MIN_BALL_Y;
-            vy = -0.75*vy;
-        } else if(y > MAX_BALL_Y)
-        {
-            y = MAX_BALL_Y;
-            vy = -0.75*vy;
+        if (is_lcd_touched()) {
+            ball.shoot_at(Position::target());
+        } else {
+            ball.tick();
         }
 
-        if (x < MIN_BALL_X) {
-            x = MIN_BALL_X;
-            vx = -0.75*vx;
-        } else if (x > MAX_BALL_X) {
-            x = MAX_BALL_X;
-            vx = -0.75*vx;
-        }
-
-        LCD.DrawCircle((int)x, (int)y, BALL_RADIUS);
+        ball.draw();
         LCD.Update();
     }
 
