f1e0f6f9b38d1932b56d59820c32d96b5c3b3d1e
wxb1ank <wxblank@gmail.com>
2022-12-03 21:07:32 -0500

    Align statistics

--- Sources/menu/page/stats.cpp
+++ Sources/menu/page/stats.cpp
@@ -2,6 +2,8 @@
 
 #include <FEHLCD.hpp>
 
+#include <sstream>
+
 namespace menu {
 
 StatsPage::StatsPage(const float centerY)
@@ -12,23 +14,19 @@ void StatsPage::run(game::Statistics &stats) {
     ui::BackgroundView(Color::BLACK).draw();
     ui::MultilineLabel(TITLE_POSITION, "Lifetime\nStatistics", Color::SILVER).draw();
 
+    std::stringstream content{};
+    content << "Total Wins:      " << stats.getWins() << '\n';
+    content << "Total Score:     " << stats.getTotalScore() << '\n';
+    content << "Top Score:       " << stats.getTopScore() << '\n';
+    content << "Orange Pegs Hit: " << stats.getOrangePegsHit() << '\n';
+    content << "Balls Shot:      " << stats.getBallsShot();
+
     const auto statLabels = ui::MultilineLabel(
         Position(static_cast<float>(Screen::CENTER_X) - 40.f, static_cast<float>(Screen::CENTER_Y)),
-        "Total Wins:\n"
-        "Total Score:\n"
-        "Top Score:\n"
-        "Orange Pegs Hit:\n"
-        "Balls Shot:",
+        content.str(),
         Color::WHITE
     );
     statLabels.draw();
-
-    // TODO
-    LCD.WriteAt(static_cast<int>(stats.getWins()), 250, 50);
-    LCD.WriteAt(static_cast<int>(stats.getTotalScore()), 250, 70);
-    LCD.WriteAt(static_cast<int>(stats.getTopScore()), 250, 90);
-    LCD.WriteAt(static_cast<int>(stats.getOrangePegsHit()), 250, 110);
-    LCD.WriteAt(static_cast<int>(stats.getBallsShot()), 250, 130);
 
     while (!this->shouldReturnFromRun());
 }
